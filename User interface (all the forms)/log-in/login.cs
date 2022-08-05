0d390f84d66f44ac32b9b545ad469c1cedd382d2
FatmaKMohamed <108635020+FatmaKMohamed@users.noreply.github.com>
2022-08-05 19:51:19 +0200

    Add files via upload

---
+++ User interface (all the forms)/log-in/login.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace library_system
+{
+    public partial class login : Form
+
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+        int count = 0;
+
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            signup f1 = new signup();
+            f1.Show();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from _User where user_name ='"+textBox1.Text+ "'and user_password ='" +textBox2.Text+ "' ";
+           
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            count = Convert.ToInt32(dt.Rows.Count.ToString());
+            if(count == 0)
+            {
+                MessageBox.Show("information does not match");
+            }
+            else
+            {
+                this.Hide();
+                user_interface us = new user_interface();
+                us.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
