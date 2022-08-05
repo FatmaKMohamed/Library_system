62e569ee5208d09985d12c5101a2b984e95b6a8e
FatmaKMohamed <108635020+FatmaKMohamed@users.noreply.github.com>
2022-08-05 20:56:50 +0200

    Add files via upload

---
+++ User interface (all the forms)/Categories/Poetry/poetry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace library_system
+{
+    public partial class poetry : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+        public poetry()
+        {
+            InitializeComponent();
+        }
+
+        private void poetry_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select *  from Item where category = 'poetry'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
