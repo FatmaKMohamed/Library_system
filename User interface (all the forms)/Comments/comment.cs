8b30b545a757b4e66f72e0120229223525ae4eaa
FatmaKMohamed <108635020+FatmaKMohamed@users.noreply.github.com>
2022-08-05 20:17:39 +0200

    Add files via upload

---
+++ User interface (all the forms)/Comments/comment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace library_system
+{
+    public partial class comment : Form
+    {
+        public comment()
+        {
+            InitializeComponent();
+        }
+
+        private void comment_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = "INSERT INTO comment values('" + textBox1.Text + "','" + textBox2.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Thanks for your opinion!");
+        }
+    }
+}
