f06e60693123b99d5251e4d2fbb8c9c260bc6a37
FatmaKMohamed <108635020+FatmaKMohamed@users.noreply.github.com>
2022-08-05 20:55:01 +0200

    Add files via upload

---
+++ User interface (all the forms)/Categories/Literature/literature.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace library_system
+{
+    public partial class literature : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+        public literature()
+        {
+            InitializeComponent();
+        }
+
+        private void literature_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select *  from Item where category = 'literature'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
