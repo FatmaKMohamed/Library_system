8d847542f88d14d54b10961f8ec59fc1a0a488fc
FatmaKMohamed <108635020+FatmaKMohamed@users.noreply.github.com>
2022-08-05 20:19:05 +0200

    Add files via upload

---
+++ User interface (all the forms)/Student form/student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace library_system
+{
+    public partial class student : Form
+    {
+        public student()
+        {
+            InitializeComponent();
+        }
+
+        private void student_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-F66NTNQ;Initial Catalog=Library_management;Integrated Security=True;Pooling=False");
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = "INSERT INTO Student values('" + textBox4.Text + "','" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Saved successfully");
+        }
+    }
+}
